e7dab987fd11b87192b39d6e813f29326b93e496
imakewebthings <caleb@imakewebthings.com>
2011-08-23 18:46:20 +0800

    Sorting out pointer events stuff so links in sample slide can be clicked

--- home.js
+++ home.js
@@ -8,4 +8,8 @@ $(function() {
 			goto: -1 // No key activation
 		}
 	});
+	
+	Modernizr.addTest('pointerevents',function(){
+		return document.documentElement.style.pointerEvents === '';
+	});
 });
