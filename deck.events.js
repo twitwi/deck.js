71b03e58d1a2013489088b54816658dbbd661a23
Mike Harris <mikeharris100@gmail.com>
2011-10-11 17:02:10 +0100

    Added extra events for when a slide loses current/prev/next status
    As per Gist git://gist.github.com/1228641.git

--- deck.events.js
+++ deck.events.js
@@ -1,11 +1,18 @@
 (function($, deck, undefined) {
    $(document).bind('deck.change', function(e, from, to) {
       var $prev = $[deck]('getSlide', to-1),
-      $next = $[deck]('getSlide', to+1);
+      $next = $[deck]('getSlide', to+1),
+      $oldprev = $[deck]('getSlide', from-1),
+      $oldnext = $[deck]('getSlide', from+1);
 
       $[deck]('getSlide', to).trigger('deck.becameCurrent');
+      $[deck]('getSlide', from).trigger('deck.lostCurrent');
+
       $prev && $prev.trigger('deck.becamePrevious');
       $next && $next.trigger('deck.becameNext');
+
+      $oldprev && $oldprev.trigger('deck.lostPrevious');
+      $oldnext && $oldnext.trigger('deck.lostNext');
    });
 })(jQuery, 'deck');
 
