09b9d08ae44c6e54c8821d62cb3dafe80bb2dc7b
Remi Emonet <remi.emonet@reverse--com.heeere>
2012-07-26 22:29:56 +0200

    [step] cleaned up, refactored, plus share some methods for toplevel slide access

--- extensions/step/deck.step.js
+++ extensions/step/deck.step.js
@@ -7,7 +7,7 @@ https://github.com/imakewebthings/deck.js/blob/master/GPL-license.txt
 */
 
 /*
-This module provides new methods for stepping without considering sub-slides.
+This module provides new methods for stepping without considering sub-slides, together with tools for finding toplevel slides etc.
 It also overrides the defaults keybinding and countNested value (so it is better to include it after "goto" and "status" extensions).
 */
 
@@ -37,51 +37,45 @@ It also overrides the defaults keybinding and countNested value (so it is better
         },
         countNested: false
     });
-    var slideTestDelayed = function(opts) {return $.map([
-        opts.classes.before,
-        opts.classes.previous,
-        opts.classes.current,
-        opts.classes.next,
-        opts.classes.after
-        ], function(el, i) {
-            return '.' + el;
-        }).join(', ');}
     var myInArray = function(el, arr) {
-        for (i in arr) if (el.is(arr[i])) return i*1; // cast to int
+        for (i in arr) if (arr[i].is(el)) return i*1; // cast to int
         return -1;
     };
-    $[deck]('extend', 'previousTopLevelSlide', function() {
+    $[deck]('extend', 'getToplevelSlideOf', function(node) {
         var opts = $[deck]('getOptions');
-        var slideTest = slideTestDelayed(opts);
-        var topLevelOf = function(node) {
-            var $parentSlides = $(node).parentsUntil(opts.selectors.container, slideTest);
-            return $parentSlides.length ? $parentSlides.last() : node;
-        };
+        var slides = $[deck]('getSlides');
+        var last = {node: $(node), index: myInArray(node, slides)};
+        $(node).parentsUntil(opts.selectors.container).each( function(ii, v) {
+            var ind = myInArray(v, slides);
+            if (ind != -1) {
+                last = {node: $(v), index: ind};
+            }
+        });
+        return last;
+    });
+    $[deck]('extend', 'getToplevelSlideOfIndex', function(ind) {
+        return $[deck]('getToplevelSlideOf', $[deck]('getSlide', ind));
+    });
+    $[deck]('extend', 'previousTopLevelSlide', function() {
         /* Find the real previous parent */
         var current = $[deck]('getSlide');
-        var currentParent = topLevelOf(current);
-        var toGo = myInArray(currentParent, $[deck]('getSlides'));
-        if (current.is(currentParent) && toGo > 0) {
+        var currentParent = $[deck]('getToplevelSlideOf', current);
+        var toGo = currentParent.index;
+        if (current.is(currentParent.node) && toGo > 0) {
             // This is already toplevel slide, just go to the previous toplevel one (parent of the previous one)
-            toGo = myInArray(topLevelOf($[deck]('getSlide', toGo-1)), $[deck]('getSlides'));
+            toGo = $[deck]('getToplevelSlideOfIndex', toGo-1).index;
         }
         $[deck]('go', toGo);
         
     });
     $[deck]('extend', 'nextTopLevelSlide', function() {
-        var opts = $[deck]('getOptions');
-        var slideTest = slideTestDelayed(opts);
-        var topLevelOf = function(node) {
-            var $parentSlides = $(node).parentsUntil(opts.selectors.container, slideTest);
-            return $parentSlides.length ? $parentSlides.last() : node;
-        };
         /* Find the real next parent */
         var current = $[deck]('getSlide');
-        var currentParent = topLevelOf(current);
-        var icur = myInArray(current, $[deck]('getSlides'));
+        var currentParent = $[deck]('getToplevelSlideOf', current);
+        var icur = currentParent.index;
         for (; icur < $[deck]('getSlides').length; icur++) {
-            var cursorParent = topLevelOf($[deck]('getSlide', icur));
-            if (!currentParent.is(cursorParent)) {
+            var cursorParent = $[deck]('getToplevelSlideOfIndex', icur).node;
+            if (!cursorParent.is(currentParent.node)) {
                 $[deck]('go', icur);
                 break;
             }
@@ -108,19 +102,14 @@ It also overrides the defaults keybinding and countNested value (so it is better
     // we will init the subslides (in case they are animations), in a backward order
     $d.bind('deck.change', function(e, from, to) {
         var opts = $[deck]('getOptions');
-        var slideTest = slideTestDelayed(opts);
-        var topLevelOf = function(node) {
-            var $parentSlides = $(node).parentsUntil(opts.selectors.container, slideTest);
-            return $parentSlides.length ? $parentSlides.last() : node;
-        };
         // consider only the case where we actually changed slide
-        if (topLevelOf($[deck]('getSlide', to)).is(topLevelOf($[deck]('getSlide', from)))) {
+        if ($[deck]('getToplevelSlideOfIndex', to).node.is($[deck]('getToplevelSlideOfIndex', from).node)) {
             return;
         }
         var direction = "forward";
         if (from > to){
             direction = "reverse";
         }
-        $(topLevelOf($[deck]('getSlide', to)).find(opts.selectors.subslidesToNotify).get().reverse()).trigger('deck.toplevelBecameCurrent', direction);
+        $($[deck]('getToplevelSlideOfIndex', to).node.find(opts.selectors.subslidesToNotify).get().reverse()).trigger('deck.toplevelBecameCurrent', direction);
     });
 })(jQuery, 'deck');
