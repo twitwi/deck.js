12c880204162aef9b2eae5effc3eaa8c27534e9c
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-03-20 02:16:50 +0100

    [incubator] autoshow: an extension for automatically going through some images/... in a widget

---
+++ extensions/incubator/autoshow.js
@@ -0,0 +1,27 @@
+
+
+$.initAutoshow = function() {
+
+$(".autoshow").each(function(){
+    $('>div:gt(0)', this).hide();
+})
+
+$('.autoshow').each(function(){
+  var t = this
+  $('>div:not(:first)', t).css('display', 'none');
+  var period = parseInt($(t).attr('data-period') || 4400);
+  var fadeIn = parseInt($(t).attr('data-time-in') || 1200);
+  var fadeOut = parseInt($(t).attr('data-time-out') || 800);
+  setInterval(function() { 
+   $('>div:first', t)
+    .fadeOut(fadeOut, function() {$(this).css('display', 'none');}) // work a round a bug/optim that keep display:block when parent is none (e.g. when slide is already hidden)
+    .next()
+    .fadeIn(fadeIn)
+    .end()
+    .appendTo(t);
+},  period);
+  });
+
+
+}
+
