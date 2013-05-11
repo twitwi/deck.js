baaecd2f883f27613f112ca05045bff776b3431f
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-05-11 20:38:54 +0200

    [incubator] event logger can now add BANG markers (with ESC) and be cleared (with Shift+ESC)

--- extensions/incubator/log-events.js
+++ extensions/incubator/log-events.js
@@ -6,17 +6,35 @@ $(document).bind('deck.init', function() {
         return;
     }
 
-    $(document).bind('deck.change', function(e, from, to) {
+    var clearStorage = function(what) {
+        localStorage.setItem("logs", "");
+        log("CLEARED BY USER");
+    }
+    var log = function(what) {
         var now = new Date();
         var time = now.toString()
-        var log = time + " " + from + " " + to;
-        //alert(log);
+        var log = time + " " + what;
         
         var data = localStorage.getItem("logs");
-        data = log+"\n"+data;
+        data = log + "\n" + data;
         localStorage.setItem("logs", data);
         $(".logs").html(data);
+    }
 
+    $(document).bind('deck.change', function(e, from, to) {
+        log(from + " " + to)
     });   
+
+    // Bind key event to add a marker in the logs
+    var $d = $(document);
+    $d.unbind('keydown.logbang').bind('keydown.logbang', function(e) {
+        var K = 27; // escape
+        if (e.which === K || $.inArray(e.which, K) > -1) {
+            log("BANG");
+            if (e.shiftKey) {
+                clearStorage();
+            }
+        }
+    });
 
 });
