726bbb5dded46d9e17bf083791387fce0644af2d
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-05-12 20:17:58 +0200

    [incubator] better time logging: add two level of relative time (last clear/last bang)

--- extensions/incubator/log-events.js
+++ extensions/incubator/log-events.js
@@ -6,23 +6,59 @@ $(document).bind('deck.init', function() {
         return;
     }
 
+    var pad = function(base, right) {
+        var s = ""+right;
+        if (s.length >= base.length) return s;
+        else return base.substring(0, base.length - s.length) + s
+    }
+    var formatTime = function(t) {
+        var min = parseInt(t / 1000 / 60);
+        var sec = parseInt(t / 1000 - 60 * min);
+        var time = pad("00", min) + ":" + pad("00", sec)
+        return time;
+    }
     var clearStorage = function(what) {
-        localStorage.setItem("logs", "");
+        localStorage.setItem("deckjs-logs", null);
         log("CLEARED BY USER");
+        localStorage.setItem("deckjs-logs-clearbase", JSON.stringify(new Date()));
+        $(".logs-clear").stop(true,true).show(200).hide(400);
+    }
+    $(".logs-clear").hide(10);
+    $(".logs-bang").hide(10);
+    var reset = function() {
+        localStorage.setItem("deckjs-logs-base", JSON.stringify(new Date()));
+    }
+    var getDateOrSet = function(k, or) {
+        var res = localStorage.getItem(k);
+        if (res == null) {
+            res = JSON.stringify(or);
+            localStorage.setItem(k, res);
+        } else {
+            res = new Date(JSON.parse(res));
+        }
+        return res;
     }
     var log = function(what) {
         var now = new Date();
         var time = now.toString()
-        var log = time + " " + what;
-        
-        var data = localStorage.getItem("logs");
+        var base = getDateOrSet("deckjs-logs-base", now);
+        var clearbase = getDateOrSet("deckjs-logs-clearbase", now);
+        var db = (now - base)/1000;
+        var dcb = (now - clearbase)/1000;
+        var dbtime = formatTime(now - base);
+        var dcbtime = formatTime(now - clearbase);
+        var log = time.replace(/GMT.*/, "") + " " + what + " " + dcb + " " + db + " " + dcbtime + " " + dbtime;
+
+        var data = localStorage.getItem("deckjs-logs");
         data = log + "\n" + data;
-        localStorage.setItem("logs", data);
+        localStorage.setItem("deckjs-logs", data);
         $(".logs").html(data);
     }
 
+    var saveCurrent = -1;
     $(document).bind('deck.change', function(e, from, to) {
-        log(from + " " + to)
+        log(from + " " + to);
+        saveCurrent = to;
     });   
 
     // Bind key event to add a marker in the logs
@@ -30,7 +66,9 @@ $(document).bind('deck.init', function() {
     $d.unbind('keydown.logbang').bind('keydown.logbang', function(e) {
         var K = 27; // escape
         if (e.which === K || $.inArray(e.which, K) > -1) {
-            log("BANG");
+            log("BANG " + saveCurrent);
+            $(".logs-bang").stop(true,true).show(200).hide(400);
+            reset();
             if (e.shiftKey) {
                 clearStorage();
             }
