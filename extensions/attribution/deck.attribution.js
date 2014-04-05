4d7be5cdfaed58ae8a7e7efac76430ee6f9bfef9
Rémi Emonet <remi.emonet@reverse--com.heeere>
2014-04-05 18:36:31 +0200

    [attribution] added the attribution extensions (automatically adds some text and a link for elements having a "data-attribution" attributes)

---
+++ extensions/attribution/deck.attribution.js
@@ -0,0 +1,45 @@
+/*!
+Deck JS - deck.attribution
+Copyright (c) 2014 Rémi Emonet
+Licensed under the MIT license.
+https://github.com/imakewebthings/deck.js/blob/master/MIT-license.txt
+*/
+
+/*
+This module automatically adds some text and a link for elements having a "data-attribution" attributes.
+*/
+
+(function($, deck, undefined) {
+    $.extend(true, $.deck.defaults, {
+        classes: {
+            attributionWrapper: 'attribution-wrap',
+            attributionLink: 'attribution-link',
+        },
+        dataAttribution: "data-attribution",
+        dataAttributionContent: "data-attribution-content",
+        dataAttributionClass: "data-attribution-class",
+    });
+
+    var $d = $(document);
+    $d.bind('deck.init', function() {
+        var o = $.deck('getOptions');
+        $('*['+o.dataAttribution+']').each(function(i, el){
+            console.log(el)
+            var c = $(el).attr(o.dataAttributionContent);
+            var cc = $(el).attr(o.dataAttributionClass);
+            var a = $(el).attr(o.dataAttribution);
+            var w = $(el).wrap("<div>").parent();
+            w.addClass(o.classes.attributionWrapper);
+            var link = $("<a>");
+            link.addClass(o.classes.attributionLink);
+            if (!! cc) link.addClass(cc);
+            link.attr("href", a);
+            if (!! c) {
+                link.html(c);
+            } else {
+                link.html(a);
+            }
+            $(el).after(link);
+        });
+    });
+})(jQuery, 'deck');
