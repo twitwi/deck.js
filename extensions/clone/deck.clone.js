905ea62496703eb80a928f4112977611cabbd000
Rémi Emonet <remi.emonet@reverse--com.heeere>
2014-04-08 00:33:34 +0200

    [clone] allow the clones to control the flow too

--- extensions/clone/deck.clone.js
+++ extensions/clone/deck.clone.js
@@ -43,10 +43,9 @@ This module provides a support for cloning the deck.
     };
 
     /*
-	jQuery.deck('addClone')
-	
-	Create a clone of this window and add it to the clones list.
-	*/
+      jQuery.deck('addClone')
+      Create a clone of this window and add it to the clones list.
+    */
     $[deck]('extend', 'addClone', function() {
         clone = new DeckClone();
         clones.push(clone);
@@ -66,44 +65,56 @@ This module provides a support for cloning the deck.
         }
         pointers.offset(pos);
     });
-      
+    
+    var isClone = false;
+    var parentDeck = null;
     /*
-        jQuery.deck('Init')
-        */
+      jQuery.deck('Init')
+    */
     $d.bind('deck.init', function() {
         var opts = $[deck]('getOptions');
         var container = $[deck]('getContainer');
         
         $(opts.selectors.clonepointer).hide();
 
-        /* Bind key events */
-        $d.unbind('keydown.deckclone').bind('keydown.deckclone', function(e) {
-            if (e.which === opts.keys.clone || $.inArray(e.which, opts.keys.clone) > -1) {
-                if (e.ctrlKey) return; // do not trigger on Ctrl+C (by default)
-                $[deck]('addClone');
-                window.___iscloner___ = true;
-                e.preventDefault();
-            }
-        });
+        isClone = window.opener && window.opener.___iscloner___;
 
-        if (window.opener && window.opener.___iscloner___) {
+        if (isClone) { // it's a clone!
             $("body").addClass(opts.classes.isClone);
             $(".anim-continue", container).removeClass("anim-continue"); // friend with anim extension
+            window.___fromparent___ = false;
+            parentDeck = function() {
+                if (! window.___fromparent___) {
+                    window.opener.$.deck.apply(window.opener.$, arguments);
+                }
+            }
+        } else { // it is a normal window
+            /* bind clone key events */
+            $d.unbind('keydown.deckclone').bind('keydown.deckclone', function(e) {
+                if (e.which === opts.keys.clone || $.inArray(e.which, opts.keys.clone) > -1) {
+                    if (e.ctrlKey) return; // do not trigger on Ctrl+C (by default)
+                    $[deck]('addClone');
+                    window.___iscloner___ = true;
+                    e.preventDefault();
+                }
+            });
         }
     })
     /* Update current slide number with each change event */
     .bind('deck.change', function(e, from, to) {
-        var opts = $[deck]('getOptions');
-        var slideTo = $[deck]('getSlide', to);
-        var container = $[deck]('getContainer');
-        cleanClones();
-        $.each(clones, function(index, clone) {
-           clone.deck('go', to);
-        });
+        if (isClone) {
+            parentDeck('go', to);
+        } else {
+            cleanClones();
+            $.each(clones, function(index, clone) {
+                clone.deck('go', to);
+            });
+        }
     })
     /* Replicate mouse cursor */
     .bind('mousemove', function(e) {
-        var current = $[deck]('getToplevelSlideOf', $[deck]('getSlide')).node; // actually uses the step extension
+        if (isClone) return;
+        var current = $[deck]('getToplevelSlideOf', $[deck]('getSlide')).node; // actually uses the step extension (dependency can be removed if needed)
         var r = current.get(0).getBoundingClientRect();
         var x = (e.clientX - r.left) / r.width;
         var y = (e.clientY - r.top) / r.height;
@@ -122,7 +133,11 @@ This module provides a support for cloning the deck.
         this.closed = function() {return clone.closed;}
         this.deck = function() {
             if (clone.closed) return;
-            if (clone['$']) clone['$'].deck.apply(clone['$'], arguments)
+            if (clone.$) {
+                clone.___fromparent___ = true;
+                clone.$.deck.apply(clone.$, arguments);
+                clone.___fromparent___ = false;
+            }
         }
     }
 })(jQuery, 'deck');
