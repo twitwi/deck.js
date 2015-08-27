919c0c3ce2ada5ba6f4257b888087ce7d3c4e7eb
Rémi Emonet <remi.emonet@reverse--com.heeere>
2015-08-28 00:56:05 +0200

    [katek] deck.js wrapper of katek
    beware of issues with firefox security http://stackoverflow.com/a/19817460/2297277
    (ideally the fonts should be in a folder of the html folder)
    workaround: preview with a webserver (e.g. python3 -m http.server)
    
    as of now, you need a special branch of katek if you want to use \mathcal etc:
    https://github.com/kevinb7/KaTeX.git
    fonts-p3_mathml
    (make)

---
+++ extensions/katex/deck.katex.js
@@ -0,0 +1,26 @@
+/*!
+Deck JS - deck.katek.js
+Copyright (c) 2015 Rémi Emonet,
+Formatting "latex" maths using the katek library (lighter than mathjax)
+*/
+
+/*
+This module provides a support for latex equation syntax.
+http://khan.github.io/KaTeX/
+*/
+
+(function($, deck, undefined) {
+    var $d = $(document);
+    var may = function(f) {return f ? f : function() {}};
+    
+    $d.bind('deck.init', function() {
+        var container = $[deck]('getContainer');
+        $('.latex', container).each(function() {
+            var it = this;
+            var v = $(it).text();
+            v = "\\displaystyle "+v+"";
+            console.log(v);
+            katex.render(v, it, {breakOnUnsupportedCmds: false});
+        });
+    });
+})(jQuery, 'deck');
