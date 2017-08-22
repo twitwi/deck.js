99035bd18b8031cc8f9e0988908f182e47b2ab7a
Rémi Emonet <remi.emonet@reverse--com.heeere>
2017-08-23 00:19:46 +0200

    [savedom] first experimental version

---
+++ extensions/savedom/deck.savedom.js
@@ -0,0 +1,67 @@
+/*!
+Deck JS - deck.savedom
+Copyright (c) 2017 Rémi Emonet
+Licensed under the MIT license.
+https://github.com/imakewebthings/deck.js/blob/master/MIT-license.txt
+*/
+
+/*
+This module adds a function (and buttons) to export the current DOM.
+The feature is useful in the process of make a single-file presentation.
+*/
+
+(function($, deck, undefined) {
+    $.extend(true, $.deck.defaults, {
+        selectors: {
+            savedomOnClick: '.save-dom-button'
+        },
+        keys: {
+            // % key
+            saveDOM: [165],
+        }
+    });
+    var $d = $(document);
+    $[deck]('extend', 'saveDOM', function(directDownload) {
+        console.log("Save DOM");
+
+        var addSaveLink = function() {
+            var content = '<!DOCTYPE html>\n<html>\n'+document.body.parentElement.innerHTML+'\n</html>';
+            var objectURL = URL.createObjectURL(new Blob([content], {type : 'text/html'}));
+            var a = document.createElement('a');
+            a.classList.add('save-dom-link');
+            a.innerText = "GO" ;
+            a.download = 'export.html';
+            a.href = objectURL;
+            a.onclick = function() {
+                setTimeout(function() {
+                    a.parentElement.removeChild(a);
+                }, 222);
+            };
+            /*
+            var refresh = function() {
+                setTimeout(function(){a.parentElement.removeChild(a);}, 800);
+                setTimeout(addSaveLink, 1000);
+            };*/
+            document.body.prepend(a);
+            //refresh();
+            if (!directDownload) {
+                a.click();
+            }
+        };
+        addSaveLink();
+    });
+    $d.bind('deck.init', function() {
+        var o = $.deck('getOptions');
+        $(o.selectors.savedomOnClick).each(function(i, el){
+            $[deck]('saveDOM');
+        });
+        $d.unbind('keydown.saveDOM').bind('keydown.saveDOM', function(e) {
+            var $opts = $[deck]('getOptions');
+            var key = $opts.keys.saveDOM;
+            if (e.which === key || $.inArray(e.which, key) > -1) {
+                e.preventDefault();
+                $[deck]('saveDOM');
+            }
+        });
+    });
+})(jQuery, 'deck');
